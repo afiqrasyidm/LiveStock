a72762294f3a77181b60f527c136042de7ee6e88
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2017-05-07 18:03:37 +0700

    ccd

--- tentang-kami.php
+++ tentang-kami.php
@@ -140,10 +140,22 @@
 		
       </div>
     </div>
+	
+	
   </div>
   <!--/.container--> 
 </section>
 
+<section id="aboutUs">
+	<div class="heading text-center"> 
+      <h2>Tempat Kami Berkarya</h2>
+    </div>
+	<div class="heading text-center"> 
+		
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.8229908132694!2d106.83478121418875!3d-6.367366995392465!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69ec13c9e63305%3A0x3b70d402caf4b8fd!2sJl.+Kedoya+Raya+No.55%2C+Pd.+Cina%2C+Beji%2C+Kota+Depok%2C+Jawa+Barat+16424!5e0!3m2!1sid!2sid!4v1494154426419" width="800" height="800" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+	</div>
+</section>
 <?php
 	include 'footer.php';
 ?>
